dfd3d9aad48108d6290198786368d365aaf9a70c
Bruno <brunoandia007@gmail.com>
2021-04-22 17:03:55 +0200

    first commit

---
+++ Fibonacci.java
@@ -0,0 +1,25 @@
+
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+
+        int numero, fibo1, fibo2, i;
+
+        numero = 10;
+
+        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:");
+
+        fibo1 = 1;
+        fibo2 = 1;
+
+        System.out.print(fibo1 + " ");
+        for (i = 2; i <= numero; i++) {
+            System.out.print(fibo2 + " ");
+            fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
+    }
+}
